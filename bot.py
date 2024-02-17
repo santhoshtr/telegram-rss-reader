51064e45fbd4f3a990d6f822d8d06f5fdc332162
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2024-02-17 22:19:06 +0530

    Update for latest telegram library

--- bot.py
+++ bot.py
@@ -3,9 +3,10 @@ import time
 import os
 
 from dotenv import load_dotenv
-from telegram import ParseMode, Update
-from telegram.ext import (CallbackContext, CommandHandler, Filters,
-                          MessageHandler, Updater)
+from telegram.constants import ParseMode
+from telegram import  Update
+from telegram.ext import (ApplicationBuilder, CallbackContext, CommandHandler, filters,
+                          ContextTypes, MessageHandler, Updater)
 
 from db import (add_feed_source, get_all_sources, get_sources,
                 is_already_present, remove_feed_source,
@@ -18,74 +19,75 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-def add_feed(update: Update, context: CallbackContext) -> None:
+async def add_feed(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_chat.id
     source = context.args[0]
     # /add https://thegradient.pub/rss
     if is_already_present(user, source):
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text=source + ' already exists.')
     else:
         add_feed_source(user, source)
         feed_info = get_feed_info(source)
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text=source + ' added.')
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text=feed_info)
 
 
-def remove_feed(update: Update, context: CallbackContext) -> None:
+async def remove_feed(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_chat.id
     source = context.args[0]
     # /remove https://thegradient.pub/rss
     if is_already_present(user, source):
         remove_feed_source(user, source)
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text=source + ' removed.')
     else:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=update.effective_chat.id, text=source + ' does not exist.')
 
 
-def list_feeds(update: Update, context: CallbackContext) -> None:
+async def list_feeds(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     userId = update.effective_chat.id
     sources = get_sources(userId)
     if len(sources):
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=userId, text="\n".join(sources))
     else:
-        context.bot.send_message(
+        await context.bot.send_message(
             chat_id=userId, text="No sources added yet")
 
-def archive_link(update: Update, context: CallbackContext):
+async def archive_link(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_chat.id
     source = context.args[0]
     url, captured = capture(source)
-    context.bot.send_message(chat_id=user, text=url)
+    await context.bot.send_message(chat_id=user, text=url)
 
-def text(update: Update, context: CallbackContext) -> None:
+async def text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_chat.id
-    context.bot.send_message(
+    text_received = update.message.text
+    await context.bot.send_message(
         chat_id=user, text='To add a feed use /add feedurl')
 
 
-def help(update: Update, context: CallbackContext) -> None:
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_chat.id
-    context.bot.send_message(
+    await context.bot.send_message(
         chat_id=user, text='To add a feed use /add feedurl')
 
 
-def hello(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'Hello {update.effective_chat.first_name}')
 
 
-def error(update, context):
+def error(update, context: ContextTypes.DEFAULT_TYPE):
     update.message.reply_text('an error occured')
     logger.error(msg="Exception while handling an update:",
                  exc_info=context.error)
 
 
-def fetch_feeds(context: CallbackContext):
+async def fetch_feeds(context: ContextTypes.DEFAULT_TYPE):
     sources = get_all_sources()
     filter_words = os.getenv('EXCLUDE_WORDS').splitlines()
     for source in sources:
@@ -111,9 +113,9 @@ def fetch_feeds(context: CallbackContext):
             if post_updated_time > last_post_updated_time:
                 last_post_updated_time = post_updated_time
             if post_updated_time > last_updated_time:
-                context.bot.send_message(chat_id=source["userId"],
-                                         text=format_feed_item(entry),
-                                         parse_mode=ParseMode.HTML)
+                await context.bot.send_message(chat_id=source["userId"],
+                                            text=format_feed_item(entry),
+                                            parse_mode=ParseMode.HTML)
                 if os.getenv('ARCHIVE_POSTS') == 'true':
                     # Add the link to archive.org
                     capture(entry.link)
@@ -124,26 +126,25 @@ def fetch_feeds(context: CallbackContext):
 
 def main():
     load_dotenv()  # take environment variables from .env.
-    updater = Updater(os.getenv('TELEGRAM_BOT_TOKEN'))
-    dispatcher = updater.dispatcher
+    application = ApplicationBuilder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
 
-    dispatcher.add_handler(CommandHandler('hello', hello))
-    dispatcher.add_handler(CommandHandler("help", help))
-    dispatcher.add_handler(CommandHandler('add', add_feed))
-    dispatcher.add_handler(CommandHandler('remove', remove_feed))
-    dispatcher.add_handler(CommandHandler('list', list_feeds))
-    dispatcher.add_handler(CommandHandler('archive', archive_link))
+    application.add_handler(CommandHandler('hello', hello))
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler('add', add_feed))
+    application.add_handler(CommandHandler('remove', remove_feed))
+    application.add_handler(CommandHandler('list', list_feeds))
+    application.add_handler(CommandHandler('archive', archive_link))
 
     # add an handler for normal text (not commands)
-    dispatcher.add_handler(MessageHandler(Filters.text, text))
+    application.add_handler(MessageHandler(filters.TEXT, text))
     # add an handler for errors
-    # dispatcher.add_error_handler(error)
+    application.add_error_handler(error)
 
-    job_queue = updater.job_queue
+    job_queue = application.job_queue
     job_queue.run_repeating(
         fetch_feeds, interval=int(os.getenv('FEED_UPDATE_INTERVAL')), first=10)
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
+
 
 
 if __name__ == '__main__':
